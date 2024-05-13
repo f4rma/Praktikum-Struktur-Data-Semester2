7d80e923202320aead3fd453628dce080803d97f
Raditya Putra Farma <145153493+f4rma@users.noreply.github.com>
2024-05-13 08:47:34 +0700

    Add files via upload

---
+++ JS-10 Shell and Quick Sort/Percobaan/Percobaan 3 JS10.c
@@ -0,0 +1,44 @@
+/*
+	Nama  : Raditya Putra Farma
+    Nim   : 23343050
+    Prodi : Informatika
+*/
+#include <stdio.h>
+
+void ShellSort(int a[], int n)
+{
+    int i, j, increment, tmp;
+    for (increment = n / 1; increment > 0; increment /= 2)
+    {
+        for (i = increment; i < 0; i++)
+        {
+            tmp = a[i];
+            for (j = i; j >= increment; j -= increment)
+            {
+                if (tmp < a[j - increment])
+                    a[j] = a[j - increment];
+                else
+                    break;
+            }
+            a[j] = tmp;
+        }
+    }
+}
+
+int main()
+{
+    int i, n, a[10];
+    printf("Masukan jumlah elemen : ");
+    scanf("%d", &n);
+    printf("Masukan %d bilangan : \n", n);
+    for (i = 0; i < n; i++)
+    {
+        scanf("%d", &a[i]);
+    }
+    ShellSort(a, n);
+    printf("Elemen yang telah diurutkan adalah:\n");
+    for (i = 0; i < n; i++)
+        printf("%d\n", a[i]);
+    printf("\n");
+    return 0;
+}
